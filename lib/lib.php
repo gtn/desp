83846b7a54ac76ef905f79a1684c6409e5139564
Dietmar Angerer <dangerer@gtn-solutions.com>
2013-05-24 16:30:25 +0200

    skilltitle from xml, not langstring anymore

--- lib/lib.php
+++ lib/lib.php
@@ -1,11 +1,11 @@
  <?php
-function get_skilltitle($skill){
-
-	try {
+function block_desp_skilltitle($skill){
+	return $skill;
+	/*try {
     $ret=get_string(strtolower(str_replace(' ', '', str_replace('ä', 'ae', str_replace('ö', 'oe', $skill)))), 'block_desp');
 	} catch (Exception $e) {
     $ret=$skill;
 	}
-	return $ret;
+	return $ret;*/
 }
 ?>
